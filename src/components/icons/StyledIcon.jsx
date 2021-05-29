cd3a24cbbf2f7666d1dc3b8637b046506d937524
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-05-29 11:06:16 -0700

    Add StyledIcon and Github icon to project
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

---
+++ src/components/icons/StyledIcon.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import styled     from 'styled-components'
+
+const Icon = ({
+  width,
+  height,
+  viewBox,
+  children,
+  className,
+}) =>
+  <svg width={width}
+       height={height}
+       className={className}
+       viewBox={viewBox}
+       version='1.1'
+       xmlns="http://www.w3.org/2000/svg"
+       xmlnsXlink="http://www.w3.org/1999/xlink">
+    {children}
+  </svg>
+
+Icon.defaultProps = {
+  width: '30',
+  height: '30',
+  viewBox: '0 0 30 30'
+}
+
+const StyledIcon = styled(Icon)`
+  & g,
+  & path,
+  & circle {
+    fill: ${props => props.theme.colors.black};
+  }
+
+  & .invert {
+    fill: ${props => props.theme.colors.white};
+  }
+`
+
+export default StyledIcon
---
+++ src/components/icons/index.js
@@ -0,0 +1,7 @@
+import StyledIcon from './StyledIcon'
+import Github from './Github'
+
+export {
+  StyledIcon,
+  Github
+}
