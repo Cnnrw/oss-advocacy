72ad5c7d5618a932fee2c58b991cfa6add22e65b
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-05-29 11:01:39 -0700

    Update _document.js
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,32 +7,25 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <meta charSet='utf-8' />
-          <link rel="apple-touch-icon"
-                sizes="180x180"
-                href="favicon/apple-touch-icon.png" />
-          <link rel="icon"
-                type="image/png"
-                sizes='32x32'
-                href="favicon/favicon-32x32.png" />
-          <link rel="icon"
-                type="image/png"
-                sizes="16x16"
-                href="favicon/favicon-16x16.png" />
-          <link rel="manifest"
-                href="favicon/site.webmanifest" />
-          <link rel="mask-icon"
-                href="favicon/safari-pinned-tab.svg"
-                color="#000000" />
-          <link rel="icon" href="favicon/favicon.ico" />
-          <meta name="msapplication-TileColor" content="#000000" />
-          <meta name="msapplication-config" content="favicon/browserconfig.xml" />
-          <meta name="theme-color" content="#000" />
           <meta name='description' content='' />
+          <meta name='keywords' content='' />
+          <link rel='apple-touch-icon' sizes='180x180' href='apple-touch-icon.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='favicon-16x16.png' />
+          <link rel='icon' href='favicon.ico' />
+          <link rel='manifest' href='site.webmanifest'/>
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='msapplication-config' content='browserconfig.xml' />
+          <meta name='theme-color' content='#ffffff' />
+          <link href='https://fonts.googleapis.com/css?family=Fira+Code:500|Archivo:700|Roboto:400,400i,700,700i&display=swap' rel='stylesheet' />
+          <link rel='mask-icon' href='safari-pinned-tab.svg' color='#000000' />
         </Head>
+
         <body>
           <Main />
           <NextScript />
         </body>
+
       </Html>
     )
   }
