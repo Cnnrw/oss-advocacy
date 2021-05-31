9424585bb34bd94b6dafa8ef39ef6267aa701acb
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-05-30 19:15:12 -0700

    Complete Footer
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,63 +1,56 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Box, Flex, Text } from 'rebass/styled-components'
+import React                                from 'react'
+import { Box, Container, Flex, Link, Text } from 'theme-ui'
 
 import GithubIcon from '@components/icons/Github'
-
-const StyledFlex = styled(Flex)`
-  padding: 3rem 4rem;
-
-  ${(props) => props.theme.mediaQueries.mobile} {
-    text-align: center;
-
-    & .icon-nav {
-      flex-wrap: wrap;
-      justify-content: center;
-    }
-  }
-  ${(props) => props.theme.mediaQueries.tablet} {
-    text-align: left;
-    & .icon-nav {
-      justify-content: flex-end;
-    }
-  }
-`
-
-const StyledLink = styled.a`
-  border-bottom: 0;
-  margin-right: 1em;
-  opacity: 0.5;
-  transition: opacity ${(props) => props.theme.animation.default};
-
-  &:hover {
-    opacity: 1;
-  }
-
-  & svg {
-    max-width: 30px;
-    max-height: 30px;
-  }
-`
 
 export const Footer = () => {
   const currentDate = new Date()
   return (
-    <Box as="footer" bg="white">
-      <StyledFlex justifyContent="space-between" flexWrap="wrap">
-        <Box width={[1, 1, 1, 1/3]} mb={3}>
-          <Text variant="label" color="black">
-            Copyright &copy; 2021-{currentDate.getFullYear()}, Connor Wilding
-          </Text>
-        </Box>
-        <Flex className='icon-nav' width={[1, 1, 1, 2/3]}>
-          <StyledLink href="https://github.com/Cnnrw"
-                      title="See my open source code contributions on Github">
-            <GithubIcon alt="Github logo" />
+    <Container as='footer' bg='white'>
+      <Flex sx={{
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        padding: '3rem 4rem 3rem 4rem',
 
-          </StyledLink>
+        '@media screen and (min-width: 320px)': {
+          textAlign: 'center',
+        },
+        '@media screen and (min-width: 768px)': {
+          textAlign: 'left',
+        }
+      }}>
+
+        <Box sx={{
+          width: ['100%', '100%', '100%', '33%'],
+          mb: 3
+        }}>
+
+          <Text variant="styles.label" color='black'>
+            Copyright &copy; {currentDate.getFullYear()}, Connor Wilding
+          </Text>
+
+        </Box>
+
+        <Flex sx={{
+          width: ['100%', '100%', '100%', '66%'],
+
+          '@media screen and (min-width: 320px)': {
+            flexWrap: 'wrap',
+            justifyContent: 'center'
+          },
+          '@media screen and (min-width: 768px)': {
+            justifyContent: 'flex-end'
+          },
+        }}>
+          <Link variant='footerLink'
+                href="https://github.com/Cnnrw"
+                title="See my open source code contributions on Github">
+            <GithubIcon alt="Github logo" />
+          </Link>
+
         </Flex>
-      </StyledFlex>
-    </Box>
+      </Flex>
+    </Container>
   )
 }
 
