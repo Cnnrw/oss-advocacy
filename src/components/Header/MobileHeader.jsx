56bbefe4f1ed5dbb1b5e16f28c568f8a045ece73
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-05-30 19:21:51 -0700

    [wip] Header
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

---
+++ src/components/Header/MobileHeader.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import Header                         from './Header'
+import debounce                       from '@utils/debounce'
+
+const MobileHeader = () => {
+  const [isMobile, setMobile] = useState(false)
+  const [isVisible, setVisibility] = useState(false)
+
+  const resize = () => {
+    let currentHideNav = window.innerWidth <= 650
+    // Art we mobile?
+    setMobile(currentHideNav)
+  }
+
+  const toggleVisibility = () => {
+    setVisibility(!isVisible)
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', debounce(resize, 250))
+      resize()
+    }
+  })
+
+  return (
+    <Header mobile={isMobile}
+            visible={isVisible}
+            toggleVisibility={toggleVisibility}
+    />
+  )
+}
+
+export default MobileHeader
