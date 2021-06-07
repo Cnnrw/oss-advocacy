ed867c1f28bbffd3c016d6127181f3eb2025279e
Connor Wilding <18753272+Cnnrw@users.noreply.github.com>
2021-06-06 17:32:47 -0700

    Add loading spinner to globe
    Signed-off-by: Connor Wilding <18753272+Cnnrw@users.noreply.github.com>

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -3,13 +3,17 @@ import { scaleSequentialSqrt }                  from 'd3-scale'
 import { interpolateYlOrRd }                    from 'd3-scale-chromatic'
 import dynamic                                  from 'next/dynamic'
 import { useEffect, useMemo, useRef, useState } from 'react'
-import { Container }                            from 'theme-ui'
+import { AspectRatio, Spinner }                 from 'theme-ui'
+import { keyframes }                            from '@emotion/react'
 import useResizeObserver                        from '@react-hook/resize-observer'
 
 import earthTexture    from 'public/static/images/earth-dark.jpg'
 import universeTexture from 'public/static/images/night-sky.png'
 
-const GlobeWrapper = dynamic(import('@components/Globe/GlobeWrapper'), { ssr: false })
+const GlobeWrapper = dynamic(
+  () => import('@components/Globe/GlobeWrapper'),
+  { ssr: false }
+)
 
 const useSize = (target) => {
   const [size, setSize] = useState()
@@ -26,21 +30,23 @@ const useSize = (target) => {
   return size
 }
 
-const Globe = (props) => {
-  const sizeRef = useRef()
-  const globeRef = useRef()
-
-  const size = useSize(sizeRef)
-
+const Globe = props => {
   const [countries, setCountries] = useState({ features: [] })
   const [populationData, setPopulationData] = useState([])
 
   const [altitude, setAltitude] = useState(0.1)
   const [transitionDuration, setTransitionDuration] = useState(1000)
 
+  const [globeInitialised, setGlobeInitialised] = useState(false)
+  const [showSpinner, setShowSpinner] = useState(true)
+
+  const sizeRef = useRef()
+  const globeRef = useRef()
+
+  const size = useSize(sizeRef)
+
   /* --- Globe Data ----------------------------------------------------------*/
   useEffect(() => {
-
     // load up-to-date population data
     fetch('/static/data/internet-usage-data.csv').then(res => res.text())
       .then(csv => csvParse(csv, ({ entity, code, population, internetUsersPercent }) => ({ entity: entity, code: code, population: +population, internetUsersPercent: +internetUsersPercent })))
@@ -55,19 +61,14 @@ const Globe = (props) => {
         setTimeout(() => {
           setTransitionDuration(4000)
           setAltitude(() => feat => Math.max(0.1, Math.sqrt(+feat.properties.population) * 7e-5))
+
+          if (globeRef.current) {
+            globeRef.current.controls().autoRotate = true;
+            globeRef.current.controls().autoRotateSpeed = 0.2;
+            globeRef.current.pointOfView({ altitude: 8 }, 2500)
+          }
         }, 3000)
       })
-  }, [])
-
-  /* --- Globe Controls ------------------------------------------------------*/
-  useEffect(() => {
-    setTimeout(() => {
-      if (globeRef.current) {
-        globeRef.current.controls().autoRotate = true;
-        globeRef.current.controls().autoRotateSpeed = 0.3;
-        globeRef.current.pointOfView({ altitude: 8 }, 2500)
-      }
-    }, 500)
   }, [])
 
   /* --- Globe size ----------------------------------------------------------*/
@@ -79,6 +80,12 @@ const Globe = (props) => {
       globeRef.current.renderer().setSize(size.width, size.height)
     }
   }, [size])
+
+  useEffect(() => {
+    setTimeout(() => {
+      setGlobeInitialised(true)
+    }, 1000)
+  }, [globeInitialised])
 
   /*
    | Add up-to-date population data to each country on first render
@@ -100,12 +107,32 @@ const Globe = (props) => {
 
   const getVal = feat => feat.properties.internetUsers / Math.max(1e4, feat.properties.population)
 
+  const fadeIn = keyframes({ from: { opacity: 0 }, to: { opacity: 1 } })
+
+
   return (
-    <Container ref={sizeRef}>
+    <AspectRatio
+      ref={sizeRef}
+      ratio={1}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        bg: '#000',
+        animation: `${fadeIn} 1.25s`,
+      }}>
+
+      {showSpinner && (
+        <Spinner title='GlobeLoading' />
+      )}
+
+      {globeInitialised && (
         <GlobeWrapper
           globeImageUrl={earthTexture}
           backgroundImageUrl={universeTexture}
           lineHoverPrecision={0}
+
+          onGlobeReady={() => setShowSpinner(false)}
 
           polygonsData={countries.features.filter(d => d.properties.iso_a2 !== 'AQ')}
           polygonAltitude={altitude}
@@ -113,19 +140,20 @@ const Globe = (props) => {
           polygonSideColor={() => 'rgba(4, 147, 114, 0.15)'}
           polygonStrokeColor={() => '#111'}
           polygonLabel={({ properties: d }) => `
-                  <b>${d.admin} (${d.iso_a2})</b>
-                  <br />
-                  Population: <i>${Math.round(+d.population / 1e4) / 1e2}M</i>
-                  <br />
-                  No. Internet Users: <i>${Math.round(d.internetUsers / 1e4) / 1e2}M</i>
-                  <br />
-                  % of pop.: <i>${parseFloat(d.internetUsersPercent).toFixed(2)}%</i>
-                `}
+                <b>${d.admin} (${d.iso_a2})</b>
+                <br />
+                Population: <i>${Math.round(+d.population / 1e4) / 1e2}M</i>
+                <br />
+                No. Internet Users: <i>${Math.round(d.internetUsers / 1e4) / 1e2}M</i>
+                <br />
+                % of pop.: <i>${parseFloat(d.internetUsersPercent).toFixed(2)}%</i>
+              `}
           polygonsTransitionDuration={transitionDuration}
-          forwardRef={globeRef}
+          forwardRef={ globeRef }
           {...props}
         />
-    </Container>
+      )}
+    </AspectRatio>
   )
 }
 
